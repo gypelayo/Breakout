db9c7d3cdf84c88ebc0e3a9c07381fd679a363fd
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-10-26 15:23:38 +0000

    Implemented loose ball and respawn mechanism

---
+++ Assets/Code/Death.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerPrefab;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ball")
+        {
+            Destroy(collision.collider.gameObject);
+            Destroy(transform.parent.GetChild(0).gameObject);
+            GameObject temp = Instantiate(playerPrefab, transform.parent.position, Quaternion.identity, transform.parent);
+            temp.transform.SetAsFirstSibling();
+        }
+    }
+}
