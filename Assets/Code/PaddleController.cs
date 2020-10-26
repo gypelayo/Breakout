e9e7d79047d81f0a37d05bc44b9b56f67660acf9
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-10-26 14:56:20 +0000

    Implemented ball bounce control mechanic

--- Assets/Code/PaddleController.cs
+++ Assets/Code/PaddleController.cs
@@ -21,12 +21,47 @@ public class PaddleController : MonoBehaviour
         Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
 
         transform.position = new Vector2(worldPosition.x, transform.position.y);
-        
-        if(Input.GetKeyDown(KeyCode.Mouse0) && !ballReleased)
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !ballReleased)
         {
             ballReleased = true;
             ball.transform.parent = null;
             ball.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 10, ForceMode2D.Impulse);
+
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ball")
+        {
+            collision.collider.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            float distToCenter = collision.transform.position.x - transform.position.x;
+            
+            if(distToCenter>=-0.25f && distToCenter < 0.25f)
+            {
+                collision.collider.GetComponent<Rigidbody2D>().AddForce(Vector2.up*10, ForceMode2D.Impulse);
+            }
+            if(distToCenter >= 0.25f && distToCenter < 0.5f)
+            {
+                collision.collider.GetComponent<Rigidbody2D>().AddForce((Vector2.up + Vector2.right) * 10, ForceMode2D.Impulse);
+            }
+            if (distToCenter >= 0.5f)
+            {
+                collision.collider.GetComponent<Rigidbody2D>().AddForce((Vector2.up + 2*Vector2.right).normalized * 10, ForceMode2D.Impulse);
+            }
+
+            if (distToCenter <= -0.25f && distToCenter > -0.5f)
+            {
+                collision.collider.GetComponent<Rigidbody2D>().AddForce((Vector2.up + Vector2.left) * 10, ForceMode2D.Impulse);
+            }
+            if (distToCenter <= -0.5f)
+            {
+                collision.collider.GetComponent<Rigidbody2D>().AddForce((Vector2.up + 2 * Vector2.left).normalized * 10, ForceMode2D.Impulse);
+            }
+
+
 
 
         }
