d94d2c23a1fe585df93cc4e5cc513c4c388ce443
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-02 15:50:09 +0000

    Created Respawn Method

--- Assets/Code/Death.cs
+++ Assets/Code/Death.cs
@@ -10,10 +10,15 @@ public class Death : MonoBehaviour
     {
         if (collision.collider.tag == "Ball")
         {
+            RespawnPaddle(collision);   
+        }
+    }
+
+    private void RespawnPaddle(Collision2D collision)
+    {
             Destroy(collision.collider.gameObject);
             Destroy(transform.parent.GetChild(0).gameObject);
             GameObject temp = Instantiate(playerPrefab, transform.parent.position, Quaternion.identity, transform.parent);
             temp.transform.SetAsFirstSibling();
-        }
     }
 }
