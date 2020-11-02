b099396672f08191a0f0176d329479ad0486f129
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-02 17:50:54 +0000

    Created configurable line of bricks

---
+++ Assets/Code/LineController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+    [SerializeField]
+    private Color lineColor;
+
+    void Start()
+    {
+        for(int childIndex=0;childIndex<transform.childCount;childIndex++)
+        {
+            transform.GetChild(childIndex).GetComponent<SpriteRenderer>().color=lineColor;
+        }
+    }
+}
