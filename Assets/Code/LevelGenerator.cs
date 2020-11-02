99550c37c07129beeacc79c55d8934dfb9bf27ed
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-02 17:51:25 +0000

    Deleted old code from level generator

--- Assets/Code/LevelGenerator.cs
+++ Assets/Code/LevelGenerator.cs
@@ -8,34 +8,7 @@ public class LevelGenerator : MonoBehaviour
     private GameObject brickPrefab;
     void Start()
     {
-        Cursor.visible = false;
-        for (int line = 0; line< 5; line++)
-        {
-            for (int column = -7; column < 8; column++)
-            {
-                GameObject tempObj = Instantiate(brickPrefab, new Vector2(column, line + 3), Quaternion.identity, transform);
-                if (line == 0)
-                {
-                    tempObj.GetComponent<SpriteRenderer>().color = Color.red;
-                }
-                if (line == 1)
-                {
-                    tempObj.GetComponent<SpriteRenderer>().color = Color.blue;
-                }
-                if (line == 2)
-                {
-                    tempObj.GetComponent<SpriteRenderer>().color = Color.yellow;
-                }
-                if (line == 3)
-                {
-                    tempObj.GetComponent<SpriteRenderer>().color = Color.green;
-                }
-                if (line == 4)
-                {
-                    tempObj.GetComponent<SpriteRenderer>().color = Color.magenta;
-                }
-            }
-        }
+        Cursor.visible=false;
     }
 
     // Update is called once per frame
