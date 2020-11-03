367194f23261fcdb0ec71f4251191fd944d566fe
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-03 18:09:28 +0000

    Implemented explosive brick

--- Assets/Code/BrickScript.cs
+++ Assets/Code/BrickScript.cs
@@ -6,14 +6,40 @@ public class BrickScript : MonoBehaviour
 {
     [SerializeField]
     private GameObject xplosionPrefab;
+    [SerializeField]
+    private bool isExplosive;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Ball")
         {
+            Destroy(gameObject);
+        }
+         if (!isExplosive)
+        {
             GameObject instantiatedXplosion = Instantiate(xplosionPrefab, transform.position, Quaternion.identity, null);
             instantiatedXplosion.GetComponent<ParticleSystem>().startColor = GetComponent<SpriteRenderer>().color;
             Destroy(instantiatedXplosion, 3);
-            Destroy(gameObject);
+        }
+        else
+        {
+            GameObject instantiatedXplosion = Instantiate(xplosionPrefab, transform.position, Quaternion.identity, null);
+            instantiatedXplosion.transform.localScale = new Vector3(1,1,1);
+            instantiatedXplosion.GetComponent<ParticleSystem>().startSize = 3;
+            instantiatedXplosion.GetComponent<ParticleSystem>().startColor = GetComponent<SpriteRenderer>().color;
+            Destroy(instantiatedXplosion, 3);
         }
     }
 }
