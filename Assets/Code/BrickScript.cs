c3f160958800717eb1bcb3ad0bf635fa15186764
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-03 12:47:28 +0000

    Implemented brick explosions

--- Assets/Code/BrickScript.cs
+++ Assets/Code/BrickScript.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class BrickScript : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject xplosionPrefab;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Ball")
         {
+            GameObject instantiatedXplosion = Instantiate(xplosionPrefab, transform.position, Quaternion.identity, null);
+            instantiatedXplosion.GetComponent<ParticleSystem>().startColor = GetComponent<SpriteRenderer>().color;
+            Destroy(instantiatedXplosion, 3);
             Destroy(gameObject);
         }
     }
