33625b6ab25620d8dd72b9168fed575735f77396
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-03 12:11:23 +0000

    Implemented death based on the number of active balls reaching zero

--- Assets/Code/Death.cs
+++ Assets/Code/Death.cs
@@ -6,19 +6,32 @@ public class Death : MonoBehaviour
 {
     [SerializeField]
     private GameObject playerPrefab;
+    [SerializeField]
+    private LevelController levelController;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Ball")
         {
-            RespawnPaddle(collision);   
+            levelController.numberOfBalls--;
+
+            if (levelController.numberOfBalls == 0)
+            {
+                RespawnPaddle(collision);
+            }
+            else
+            {
+                Destroy(collision.collider.gameObject);
+            }
         }
     }
 
     private void RespawnPaddle(Collision2D collision)
     {
-            Destroy(collision.collider.gameObject);
-            Destroy(transform.parent.GetChild(0).gameObject);
-            GameObject temp = Instantiate(playerPrefab, transform.parent.position, Quaternion.identity, transform.parent);
-            temp.transform.SetAsFirstSibling();
+        Destroy(collision.collider.gameObject);
+        Destroy(transform.parent.GetChild(0).gameObject);
+        GameObject temp = Instantiate(playerPrefab, transform.parent.position, Quaternion.identity, transform.parent);
+        temp.transform.SetAsFirstSibling();
+
+
     }
 }
