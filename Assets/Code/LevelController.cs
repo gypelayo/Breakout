30e0fcadd25d596e1f77011cd93b27161b59a5cc
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-03 12:05:31 +0000

    Added Level controller

--- Assets/Code/LevelController.cs
+++ Assets/Code/LevelController.cs
@@ -1,30 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour
 {
     public int numberOfBalls;
     public int lifeCount;
-
+    [SerializeField]
+    private Text text;
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
     /// any of the Update methods is called the first time.
     /// </summary>
     void Start()
     {
-        numberOfBalls=1;
-        lifeCount=5;
+        numberOfBalls = 1;
+        lifeCount = 5;
     }
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
     void Update()
     {
-        if (numberOfBalls<=0){
-            lifeCount--;
-        }
-        if(lifeCount<0){
+        text.text = lifeCount.ToString();
+
+        if (lifeCount < 0)
+        {
             Application.Quit();
         }
     }
