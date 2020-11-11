536deeb27eb797627d07a0e6c7331e0ec9ed7f44
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-11 11:09:22 +0000

    TDD Breakout: Setup test environment and created tests and implementation for brick size

---
+++ Assets/Code/New Code/Brick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick
+{
+    private float size;
+    public Brick()
+    {
+        this.size = 2;
+    }
+    public Brick(float size)
+    {
+        this.size = size;
+    }
+    public float Size
+    {
+        get
+        {
+            return size;
+        }
+        private set
+        {
+
+        }
+    }
+}
---
+++ Assets/Tests/BrickTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BrickTests
+    {
+        [Test]
+        public void _0_Cretating_New_Brick_Sets_Size_To_2()
+        {
+            Brick brick = new Brick();
+            Assert.AreEqual(2, brick.Size);
+        }
+        [Test]
+        public void _1_Creating_New_Brick_With_Size_3_Sets_Size_3()
+        {
+            Brick brick = new Brick(3);
+            Assert.AreEqual(3, brick.Size);
+        }
+    }
+}
