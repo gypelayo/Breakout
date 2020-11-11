9cd881dab724d7ff108f8de40eb2c983c33aa41b
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-11 14:28:16 +0000

    TDD Breakout - Implemented brick existance in game mode

--- Assets/Code/New Code/GameController.cs
+++ Assets/Code/New Code/GameController.cs
@@ -13,11 +13,13 @@ public class GameController : MonoBehaviour
     private void Start()
     {
         brick1 = new Brick();
+        bricks = new List<Brick>();
         bricks.Add(brick1);
-       /* foreach (Brick brick in bricks)
+        foreach (Brick brick in bricks)
         {
             GameObject brickGameObject = Instantiate(brickDefault, transform.position, Quaternion.identity, null);
+            brickGameObject.transform.localScale = new Vector2(brick.Size,1);
             gameObjects.Add(brickGameObject);
-        }*/
+        }
     }
 }
