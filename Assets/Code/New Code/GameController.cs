9ff0e0e7d0c0cfd2f806acd1032a5cae616af83d
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-11 12:18:10 +0000

    TDD Breakout - Added instantiator of bricks to scene

--- Assets/Code/New Code/GameController.cs
+++ Assets/Code/New Code/GameController.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
 
+    public List<GameObject> gameObjects;
+    private List<Brick> bricks;
+    private Brick brick1;
+    [SerializeField]
+    private GameObject brickDefault;
+    private void Start()
+    {
+        brick1 = new Brick();
+        bricks.Add(brick1);
+       /* foreach (Brick brick in bricks)
+        {
+            GameObject brickGameObject = Instantiate(brickDefault, transform.position, Quaternion.identity, null);
+            gameObjects.Add(brickGameObject);
+        }*/
+    }
 }
