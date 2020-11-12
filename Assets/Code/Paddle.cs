ae3aad7cd00f93960507ed0b159dfbe8c4139e9c
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-12 12:42:49 +0000

    TDD Breakout - Created and implemented test for paddle creation

---
+++ Assets/Code/Paddle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle
+{
+    private float horizontalSize;
+    private float verticalSize;
+    private Color color;
+    public Paddle()
+    {
+        horizontalSize = 2;
+        verticalSize = 0.5f;
+    }
+    public Paddle(float horizontalSize)
+    {
+        this.horizontalSize = horizontalSize;
+    }
+    public float HorizontalSize
+    {
+        get
+        {
+            return horizontalSize;
+        }
+        set
+        {
+
+        }
+    }
+    public Color Color
+    {
+        get
+        {
+            return color;
+        }
+        set
+        {
+
+        }
+    }
+}
---
+++ Assets/Tests/PaddleTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class PaddleTests
+    {
+        [Test]
+        public void _0_Creating_Paddle_Sets_Horizontal_Size_To_2()
+        {
+            Paddle paddle = new Paddle();
+            Assert.AreEqual(2, paddle.HorizontalSize);
+        }
+        [Test]
+        public void _1_Creating_Paddle_With_Size_Sets_Horizontal_Size()
+        {
+            Paddle paddle = new Paddle(4);
+            Assert.AreEqual(4, paddle.HorizontalSize);
+        }
+    }
+}
