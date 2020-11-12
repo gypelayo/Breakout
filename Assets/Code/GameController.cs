81d5c09448d122c6d753052e16337b4bb67214ba
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-12 15:20:38 +0000

    TDD Breakout - Implemented paddle movement with mouse and life system

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public List<Brick> bricks;
-    public int numberOfBricks=0;
+    public List<Paddle> paddles;
+    public int numberOfPaddlesLeft;
+    public int numberOfBricks = 0;
     private void Awake()
     {
+        numberOfPaddlesLeft = 5;
         bricks = new List<Brick>();
+        paddles = new List<Paddle>();
+        for (int i = 0; i < numberOfPaddlesLeft; i++)
+        {
+            Paddle paddle = new Paddle();
+            paddles.Add(paddle);
+
+            if (i == numberOfPaddlesLeft - 1)
+            {
+                paddle.IsActive = true;
+            }
+        }
+
     }
 }
---
+++ Assets/Code/PaddleMonobehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMonobehaviour : MonoBehaviour
+{
+    private Paddle paddle;
+    private GameController gameController;
+    [SerializeField]
+    private float moveSpeed;
+    private Vector3 mousePosition;
+    private Rigidbody2D rb;
+    private Vector2 position;
+    private void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        paddle = gameController.paddles[gameController.paddles.Count - 1];
+
+        //Init mouse variables
+        moveSpeed = 0.1f;
+        rb = GetComponent<Rigidbody2D>();
+        position = transform.position;
+    }
+    private void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        position = new Vector2(position.x, paddle.Position.y);
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(position);
+    }
+    private void OnDestroy()
+    {
+        gameController.paddles[gameController.paddles.Count - 2].IsActive = true;
+        gameController.paddles.Remove(paddle);
+    }
+}
