7b1d98cfc5374ec7f4facd578db47f8159392326
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-16 18:39:24 +0000

    TDD Breakout: Homepage - Created buttons for game, options, exit and score

---
+++ Assets/Code/UI Code/ButtonMonobehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ButtonMonobehaviour : MonoBehaviour
+{
+    private Image image;
+    private void OnMouseEnter()
+    {
+        GetComponent<Image>().tintColor = Color.white;
+    }
+}
