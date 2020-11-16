ccaa714dff136b9237eb448efa84cc6366036b3a
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-16 16:04:06 +0000

    TDD Breakout - Moved powerup responsibility to the powerup gameobject

--- Assets/Code/PaddleMonobehaviour.cs
+++ Assets/Code/PaddleMonobehaviour.cs
@@ -35,7 +35,6 @@ public class PaddleMonobehaviour : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         ReflectBall(other);
-        ActivatePowerUp(other);
     }
 
     private void VariableInit()
@@ -54,7 +53,7 @@ public class PaddleMonobehaviour : MonoBehaviour
         position = new Vector2(position.x, paddle.Position.y);
         paddle.Position = position;
     }
-    private void ShootBall()
+    public void ShootBall()
     {
         ballObject.transform.parent = null;
         ballObject.AddComponent<Rigidbody2D>();
@@ -69,19 +68,7 @@ public class PaddleMonobehaviour : MonoBehaviour
         {
             collision.collider.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             float distToCenter = collision.transform.position.x - transform.position.x;
-            collision.collider.GetComponent<Rigidbody2D>().AddForce((Vector2.up + new Vector2(distToCenter, 0).normalized).normalized * 10, ForceMode2D.Impulse);
+            collision.collider.GetComponent<Rigidbody2D>().AddForce((Vector2.up + new Vector2(distToCenter / transform.localScale.x, 0).normalized).normalized * 10, ForceMode2D.Impulse);
         }
-    }
-    private void ActivatePowerUp(Collision2D collision)
-    {
-        if (collision.collider.tag == "Powerup")
-        {
-            if (collision.gameObject.GetComponent<PowerupMonobehaviour>().powerupId == 1)
-            {
-                gameObject.transform.localScale = new Vector2(4,0.5f);
-            }
-            Destroy(collision.gameObject);
-        }
-
     }
 }
--- Assets/Code/PowerupMonobehaviour.cs
+++ Assets/Code/PowerupMonobehaviour.cs
@@ -5,4 +5,29 @@ using UnityEngine;
 public class PowerupMonobehaviour : MonoBehaviour
 {
     public int powerupId;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.transform.childCount != 0)
+            {
+                other.GetComponent<PaddleMonobehaviour>().ShootBall();
+            }
+            ApplyEffect(powerupId, other);
+            Destroy(gameObject);
+        }
+    }
+    private void ApplyEffect(int powerupId, Collider2D other)
+    {
+        switch (powerupId)
+        {
+            case 1:
+                other.transform.localScale = new Vector2(4, 0.5f);
+                break;
+            case 2:
+                other.transform.localScale = new Vector2(1, 0.5f);
+                break;
+
+        }
+    }
 }
