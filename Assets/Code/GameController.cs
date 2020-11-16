2711233b31b0171c6ddf5192783581ac1c251aad
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-16 11:19:54 +0000

    TDD Breakout - Formatted code

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -6,15 +6,31 @@ public class GameController : MonoBehaviour
 {
     public List<Brick> bricks;
     public List<Paddle> paddles;
+    [SerializeField]
     public int numberOfPaddlesLeft;
     public int numberOfBricks = 0;
     [SerializeField]
     private GameObject paddlePrefab;
     private void Awake()
     {
+        Cursor.visible = false;
+
         numberOfPaddlesLeft = 5;
+
         bricks = new List<Brick>();
         paddles = new List<Paddle>();
+
+        CreatePaddleList();
+        SpawnNewPaddle();
+
+    }
+    private void SpawnNewPaddle()
+    {
+        GameObject firstPaddle = Instantiate(paddlePrefab, paddles[numberOfPaddlesLeft - 1].Position, Quaternion.identity, transform);
+        firstPaddle.GetComponent<SpriteRenderer>().color = paddles[numberOfPaddlesLeft - 1].Color;
+    }
+    private void CreatePaddleList()
+    {
         for (int i = 0; i < numberOfPaddlesLeft; i++)
         {
             Paddle paddle = new Paddle();
@@ -25,12 +41,5 @@ public class GameController : MonoBehaviour
                 paddle.IsActive = true;
             }
         }
-        SpawnNewPaddle();
-        Cursor.visible=false;
-    }
-    public void SpawnNewPaddle()
-    {
-        GameObject firstPaddle = Instantiate(paddlePrefab, paddles[numberOfPaddlesLeft - 1].Position, Quaternion.identity, transform);
-        firstPaddle.GetComponent<SpriteRenderer>().color = paddles[numberOfPaddlesLeft - 1].Color;
     }
 }
