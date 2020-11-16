8a0bcc7b87176259a23d08fc1c83b463e5af5de5
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-16 14:35:39 +0000

    TDD Breakout - Created powerup class

---
+++ Assets/Code/Powerup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup
+{
+    private int id;
+    public Powerup()
+    {
+        id = 0;
+    }
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+        set
+        {
+            id = value;
+        }
+    }
+}
---
+++ Assets/Tests/PowerupTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PowerupTests
+    {
+        [Test]
+        public void _0_Default_Id_Is_0()
+        {
+            Powerup powerup = new Powerup();
+            Assert.AreEqual(0, powerup.Id);
+        }
+        [Test]
+        public void _1_Setting_Id_Sets_Id()
+        {
+            Powerup powerup = new Powerup();
+            powerup.Id = 2;
+            Assert.AreEqual(2, powerup.Id);
+        }
+    }
+}
