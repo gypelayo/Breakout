dcb523325d3fe3d854c68fbaa433c67f59eb38d9
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-17 10:43:21 +0000

    TDD Breakout: Homepage - Created button controller monobehaviour

---
+++ Assets/Code/UI Code/OptionsMonobehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.Events;
+
+public class OptionsMonobehaviour : MonoBehaviour
+{
+    public void StartGame()
+    {
+
+    }
+    public void ExitGame()
+    {
+
+    }
+    public void GoToOptions()
+    {
+
+    }
+    public void GoToScore()
+    {
+
+    }
+}
