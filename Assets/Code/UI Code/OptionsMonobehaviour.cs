bc14921d9b5b6727675ff13e7e61346ca8af1dcc
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-17 11:35:07 +0000

    TDD Breakout: Homepage - Implemented menu options

--- Assets/Code/UI Code/OptionsMonobehaviour.cs
+++ Assets/Code/UI Code/OptionsMonobehaviour.cs
@@ -3,23 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.Events;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OptionsMonobehaviour : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject mainMenu;
+    [SerializeField]
+    private GameObject optionsMenu;
+    [SerializeField]
+    private GameObject storeMenu;
+
     public void StartGame()
     {
 
+        SceneManager.LoadScene("Breakout", LoadSceneMode.Single);
     }
     public void ExitGame()
     {
 
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
     }
     public void GoToOptions()
     {
 
+        Debug.Log("Not implemented");
     }
     public void GoToScore()
     {
 
+        mainMenu.SetActive(false);
+        storeMenu.SetActive(true);
+    }
+    public void ReturnToMainMenu()
+    {
+        storeMenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
