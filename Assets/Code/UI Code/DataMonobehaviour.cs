f17eb60943f910c0c763a4f37d2cab1c38cbb79a
Guilherme Pelayo <guilherme.pelayo@exaud.com>
2020-11-17 15:34:45 +0000

    TDD Breakout: Currency - Added balance save

---
+++ Assets/Code/UI Code/DataMonobehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataMonobehaviour : MonoBehaviour
+{
+    private Account account;
+    private Text balanceText;
+    private
+    void Start()
+    {
+        account = new Account();
+        account.AddFunds(PlayerPrefs.GetInt("Account Balance"));
+        balanceText = GetComponent<Text>();
+        balanceText.text = account.Balance.ToString() + "€";
+    }
+
+}
